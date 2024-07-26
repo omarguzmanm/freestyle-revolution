900259e6c3acaa6277c7e3fd28f2de0652c336cf
Omar Alejandro Guzmán Munguía <omaralexguzmanm21@gmail.com>
2024-07-26 00:07:34 -0600

    feat: added index view to auth users and restriction for auth pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,8 @@ import AuthView from '../views/AuthView.vue'
 import NewsView from '../views/NewsView.vue'
 import TournamentsLiveView from '../views/TournamentsLiveView.vue'
 import CompetitorsView from '../views/CompetitorsView.vue'
+import IndexAuthView from '../views/auth/IndexView.vue'
+import { useAuthStore } from '@/stores/auth'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -38,12 +40,29 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+
+  //User logged routes
+  {
+    path: '/index',
+    name: 'index',
+    component: IndexAuthView,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach(async (to) => {
+  const publicPages = ['/login', '/register', '/home', '/news', '/tournaments-live', '/competitors']
+  const authRequired = !publicPages.includes(to.path)
+  const auth = useAuthStore()
+  if (authRequired && !auth.user) {
+    auth.returlUrl = to.fullPath
+    return '/login'
+  }
 })
 
 export default router
