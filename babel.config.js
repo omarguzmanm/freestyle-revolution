e1c6a906885becea6d638a4ad4e638a87863a542
Omar Alejandro Guzmán Munguía <omaralexguzmanm21@gmail.com>
2024-07-29 01:40:28 -0600

    build: added fluent icon package

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+  presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: [
+    [
+      "babel-plugin-transform-imports",
+      {
+        "@vicons/fluent": {
+          transform: "@vicons/fluent/lib/${member}",
+          preventFullImport: true,
+        },
+      },
+    ],
+  ],
+};
